fce78a3b4c631959cefe3109cdb17c79190fdee5
Morten <fannoj@gmail.com>
2013-11-17 11:07:43 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config['db_host'] = "mydb1.surf-town.net";
+$config['db_username'] = "fanno_fanno";
+$config['db_password'] = "asdfg12345";
+$config['db_name'] = "fanno_test";
+$config['db_table_prefix'] = "flv_";
+$config['block_counter'] = 2;
